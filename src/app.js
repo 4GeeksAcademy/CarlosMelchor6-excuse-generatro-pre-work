b3e99ea9cdb27198c205739691fb737445b077b2
Carlos Melchor Perez <carloos0665@gmail.com>
2025-02-20 04:05:26 +0000

    add excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+ let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+ let action = ['ate', 'peed', 'crushed', 'broke'];
+ let what = ['my homework', 'my phone', 'the car'];
+ let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+  function elementRandom(array) {
+    return array[Math.floor(Math.random()* array.length)];
+  }
+  let excuseGenerator = `${elementRandom(who)} ${elementRandom(action)} ${elementRandom(what)} ${elementRandom(when)}`
+  
+  document.getElementById("excuse").innerText = excuseGenerator;
+  return excuseGenerator
 };
